68a5e144c5dfb93d339ca9d46ceb572a4bc67f60
Trevor Redfern <trevorredfern@gmail.com>
2016-04-14 16:13:41 -0700

    Adding in traits for races

---
+++ Assets/Characters/Trait.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using YamlDotNet.RepresentationModel;
+using System.Collections;
+using System.Collections.Generic;
+using ShortLegStudio;
+
+namespace ShortLegStudio.RPG.Characters {
+	public class Trait {
+		//Static Values
+		const string TRAIT_DATA_FILE = "Data/traits.yml";
+		static IList<Trait> _Traits;
+
+		public string Name { get; set; }
+		public string Description { get; set; }
+
+		public static IList<Trait> LoadFromYaml(YamlNodeWrapper yaml) {
+			var traits = new List<Trait> ();
+
+			foreach (var traitNode in yaml.Children()) {
+				var trait = new Trait ();
+				trait.Name = traitNode.GetString ("name"); 
+				trait.Description = traitNode.GetString ("description");
+				traits.Add (trait);
+			}
+
+			return traits;
+		}
+
+		public static IList<Trait> GetTraits() {
+			if (_Traits == null) {
+				var yaml = FileHelper.OpenYaml (TRAIT_DATA_FILE);
+				_Traits = LoadFromYaml (yaml);
+			}
+
+			return _Traits;
+		}
+	}
+
+
+}
---
+++ Assets/Editor/Characters/TraitTests.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+using System.IO;
+using NUnit.Framework;
+using ShortLegStudio;
+using ShortLegStudio.RPG.Characters;
+using YamlDotNet.RepresentationModel;
+using System.Text;
+
+[TestFixture]
+public class TraitTests {
+	YamlStream yaml;
+	Trait darkvision;
+	Trait hardy;
+	Trait halflingLuck;
+
+
+	[SetUp]
+	public void SetUp() {
+		var input = new StringReader(TraitYamlFile);
+
+		yaml = new YamlStream();
+		yaml.Load(input);
+		var yamlNode = new YamlNodeWrapper(yaml.Documents [0].RootNode);
+		var traits = Trait.LoadFromYaml (yamlNode);
+		darkvision = traits.First (x => x.Name == "Darkvision");
+		hardy = traits.First (x => x.Name == "Hardy");
+		halflingLuck = traits.First (x => x.Name == "Halfling Luck");
+	}
+		
+    [Test]
+    public void LoadTraitYamlFile() {
+		Assert.IsNotNull (darkvision);
+		Assert.IsNotNull (hardy);
+		Assert.IsNotNull (halflingLuck);
+    }
+
+	[Test]
+	public void TraitsHaveADescription() {
+		Assert.AreEqual ("See in the dark.", darkvision.Description);
+		Assert.AreEqual ("Really tough", hardy.Description);
+	}
+
+	private const string TraitYamlFile = @"--- 
+- trait: 
+  name: Darkvision
+  description: See in the dark.
+- trait:
+  name: Hardy
+  description: Really tough
+- trait:
+  name: Halfling Luck
+  description: Savings throw bonus
+...";
+}
