9976d0b4f80766fb48fe3eea03dc94d54ffb2ec8
Trevor Redfern <trevorredfern@gmail.com>
2016-08-20 14:34:05 -0700

    An age generator is created

---
+++ Assets/Editor/ShortLegStudio/RPG/Actions/CharacterGenerator/AssignAgeTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using NUnit.Framework;
+using ShortLegStudio.RPG.Characters;
+using ShortLegStudio.Dice;
+using System.Linq;
+using ShortLegStudio.Enchilada;
+using System.Collections.Generic;
+using ShortLegStudio.RPG.Mechanics.CharacterGenerator;
+using ShortLegStudio.RPG.Actions.CharacterGenerator;
+
+namespace RPG.Mechanics.CharacterGenerator {
+	[TestFixture]
+	public class AssignAgeTests {
+        [Test]
+        public void AssignsAnAgeToACharacterBasedOnClassAndMaturity()
+        {
+            var cls = ClassDevelopmentAge.Young;
+            var maturity = new Maturity();
+            maturity.Adulthood = 15;
+            maturity.Young = DiceStrings.ParseDice("1d4");
+
+            var assignAges = new AssignAge();
+            var age = assignAges.RandomAge(cls, maturity);
+            Assert.GreaterOrEqual(age, 16);
+            Assert.LessOrEqual(age, 19);
+        }
+	}
+}
+
---
+++ Assets/ShortLegStudio/RPG/Characters/Maturity.cs
@@ -0,0 +1,23 @@
+﻿// //-----------------------------------------------------------------------
+// // <copyright file="Maturity.cs" company="Short Leg Studio, LLC">
+// //     Copyright (c) Short Leg Studio, LLC. All rights reserved.
+// // </copyright>
+// //-----------------------------------------------------------------------
+using System;
+using ShortLegStudio.Dice;
+
+namespace ShortLegStudio.RPG.Characters
+{
+    public class Maturity
+    {
+        public Maturity()
+        {
+        }
+
+        public int Adulthood { get; set; }
+        public Cup Young { get; set; }
+        public Cup Trained { get; set; }
+        public Cup Studied { get; set; } 
+    }
+}
+
